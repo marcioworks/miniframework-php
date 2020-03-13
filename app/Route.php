ef9c77c991fe6a776a200650b8b8d734b49fdb2a
Marcio da Silva Santos <marcio.santos@fortbrasil.com.br>
2020-03-13 11:51:40 -0300

    refactoring Routes

--- app/Route.php
+++ app/Route.php
@@ -1,21 +1,10 @@
 <?php
 
 namespace App;
+use MF\Init\BootsTrap;
 
-class Route
-{
-    private $routes;
-    public function __construct() {
-        $this->initRoutes();
-        $this->run($this->getUrl());
-    }
-    public function getRoutes(){
-        return $this->routes;
-    }
-
-    public function setRoutes(array $routes){
-        $this->routes = $routes;
-    }
+class Route extends BootsTrap {
+   
     public function initRoutes(){
         $routes['home'] = array(
             'route' => '/',
@@ -31,20 +20,5 @@ class Route
         $this->setRoutes($routes);
     }
 
-    public function run($url){
-       
-        foreach ($this->getRoutes() as $key => $route) {
-            if($url == $route['route']){
-                $class = "App\\Controllers\\".ucfirst($route['controller']);
-
-                $controller = new $class;
-                $action = $route['action'];
-                $controller->$action();
-            }
-        }
-
-    }
-    public function getUrl(){
-        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    }
+   
 }
