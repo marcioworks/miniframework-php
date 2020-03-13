6c6fdde9fc2cd4933dbf03efe83d63f800856583
Marcio da Silva Santos <marcio.santos@fortbrasil.com.br>
2020-03-13 11:38:41 -0300

    router and index controller

---
+++ app/Controllers/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controllers;
+
+class IndexController {
+    public function index(){
+        echo('chegou ao indexController e disparou a action index');
+    }
+
+    public function sobreNos(){
+        echo('chegou ao indexController e disparou a action sobreNos');
+    }
+}
+?>
