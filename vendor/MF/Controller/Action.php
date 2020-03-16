7a641aa79d639e4327e6c7763fb802bf560dc8dc
Marcio da Silva Santos <marcio.santos@fortbrasil.com.br>
2020-03-16 09:40:23 -0300

    ???

--- vendor/MF/Controller/Action.php
+++ vendor/MF/Controller/Action.php
@@ -8,19 +8,13 @@ abstract class Action {
         $this->view = new \stdClass();
     }
 
-    protected function render($view, $layout){
-        $this->view->page = $view;
-        if(file_exists( "../app/Views/".$layout.".phtml")){
-            require_once "../app/Views/".$layout.".phtml";
-        }else{
-            $this->content();
-        }
-    }
-    protected function content(){
+    protected function render($view){
         $classeAtual = \get_class($this);
         $classeAtual = \str_replace('App\\Controllers\\','',$classeAtual);
         $classeAtual = strtolower(\str_replace('Controller','',$classeAtual));
-        require_once "../app/Views/".$classeAtual."/".$this->view->page.".phtml";
+       
+
+        require_once "../app/Views/".$classeAtual."/".$view.".phtml";
     }
 }
 
